d753d241325accd7fa67eb6c71ff01d65eda1b65
Aruan Macena <professoraruan@gmail.com>
2026-06-05 14:30:15 -0300

    crianco as classes conta e conta corrente do projeto

--- Projetos/sistema_bancario_poo.py
+++ Projetos/sistema_bancario_poo.py
@@ -1,53 +1,86 @@
 from abc import ABC, abstractmethod
 
 class Conta:
-    def __init__(self, saldo, numero,agencia,cliente,historico):
-        self._saldo = saldo
+    def __init__(self, numero,cliente):
+        self._saldo = 0
         self._numero = numero
-        self._agencia = agencia
+        self._agencia = "0001"
         self._cliente = cliente
-        self._historico = historico
+        self._historico = Historico()
     
+    @property
     def saldo(self):
-        pass
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
 
-    def sacar(self):
-        pass
+    def sacar(self, valor):
+        saldo = self._saldo
+        excedeu_saldo = valor > saldo
 
-    def depositar(self):
-        pass
+        if excedeu_saldo:
+            print(f"Operação falhou! Saldo insuficiente na conta {self.__class__.__name__}")
+        
+        elif valor > 0:
+            self._saldo -= valor
+            print("Saque realizado!")
+            return True
+        
+        else:
+            print("Valor informado inválido")
+        return False
+
+    def depositar(self,valor):
+        
+        if valor>0:
+            self._saldo += valor
+            print("Depositado com sucesso!")
+            return True
+        else:
+            print("Falha ao depositar")
+            return False
 
     @classmethod
     def nova_conta(cls, cliente, numero):
-        return cls(0,numero,"0001",cliente,[])
+        return cls(numero,cliente,)
 
-
-#a = Conta.nova_conta("Aruan",1)
-
-#print(f"nome:{a._cliente}, nuemro da conta:{a._numero}, agencia: {a._agencia}, saldo:{a._saldo}, historico:{a._historico}")
 
 class Conta_corrente(Conta):
-    def __init__(self, saldo, numero, agencia, cliente, historico, limite=100,limite_saques=4):
-        super().__init__(saldo, numero, agencia, cliente, historico)
+    def __init__(self, numero, cliente, limite=100,limite_saques=4):
+        super().__init__(numero, cliente)
         self._limite = limite
         self._limite_saques = limite_saques
 
 
 
 class Cliente:
-    def __init__(self,endereco,contas=[]):
+    def __init__(self,endereco):
         self._endereco = endereco
-        self._contas = contas
+        self._contas = [] #inicializacao do cliente sem contas (vazio)
     
-    def adicionar_conta(self):
-        pass
+    def adicionar_conta(self, conta):
+        self._contas.append(conta)
 
-    def realizar_transacao(self):
-        pass
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
 
 class Pessoa_Fisica(Cliente):
-    def __init__(self,endereco, contas, cpf, nome, data_nascimento):
-        super().__init__(endereco, contas)
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(endereco)
         self._cpf = cpf
         self._nome = nome
         self._data_nascimento = data_nascimento
