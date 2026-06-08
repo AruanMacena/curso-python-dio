8925ad940ef158cc1fa994de4add92562c46af73
Aruan Macena <professoraruan@gmail.com>
2026-06-08 13:49:50 -0300

    Contrucao da funcao para adicionar clientes

--- Projetos/sistema_bancario_poo.py
+++ Projetos/sistema_bancario_poo.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
+import textwrap
 
 class Conta:
     def __init__(self, numero,cliente):
@@ -58,14 +60,38 @@ class Conta:
     def nova_conta(cls, cliente, numero):
         return cls(numero,cliente,)
 
-
 class Conta_corrente(Conta):
     def __init__(self, numero, cliente, limite=100,limite_saques=4):
         super().__init__(numero, cliente)
         self._limite = limite
         self._limite_saques = limite_saques
 
+    def sacar(self, valor):
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao["Tipo"]==Saque.__name__]
 
+        )
+
+        excedeu_limite = valor>self._limite
+        excedeu_saques = numero_saques >= self._limite_saques
+
+        if excedeu_limite:
+            print("Falha! Valor de saque maior que limite disponível por saque")
+        
+        elif excedeu_saques:
+            print(f"Numero maximo de saques foi superado ao de {self._limite_saques}")
+        
+        else:
+            return super().sacar(valor)
+        
+        return False
+    
+    def __str__(self):
+        return f"""\
+            Agência:\t {self._agencia}
+            C/C:\t\t{self._numero}
+            Titular:\t{self._cliente._nome}
+        """
 
 class Cliente:
     def __init__(self,endereco):
@@ -87,18 +113,115 @@ class Pessoa_Fisica(Cliente):
 
 class Transacao(ABC):
 
+    @property
     @abstractmethod
-    def registrar(self):
+    def valor(self):
         pass
 
-class Deposito:
-    def depositar(self, valor):
-        pass #verificar condicoes para deposito e operacao de deposito
+    @classmethod
+    @abstractmethod
+    def registrar(self, conta):
+        pass
 
-class Saque:
-    def sacar(self):
-        pass #condicoes de saque e operacoes de saque
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+    
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
 
 class Historico:
-    def adicionar_transacao(self):
-        pass
+
+
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "Tipo": transacao.__class__.__name__,
+                "Valor": transacao.valor,
+                "Data": datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+
+            }
+        )
+
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [n]\tNova conta
+    [l]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
+
+def main():
+    clientes = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            depositar(clientes)
+
+        elif opcao == "s":
+            sacar(clientes)
+
+        elif opcao == "e":
+            exibir_extratos(clientes)
+
+        elif opcao == "n":
+            numero_conta = len(contas) +1
+            criar_conta(numero_conta, clientes, contas)
+
+        elif opcao == "l":
+            listar_contas(contas)
+
+        elif opcao == "nu":
+            criar_clientes(clientes)
+        
+        elif opcao == "q":
+            break
+
+        else:
+            print(
+                """
+                Opção inválida, digite a operação desejada
+                """
+
+            )
+
+main()
